fc3e09a8a9718853809ad3d97d70362e112a11ef
yochanchan <arai0201@gmail.com>
2025-11-24 09:28:57 +0900

    NEXT_PUBLIC_API_ENDPOINT

--- app/lib/api-client.ts
+++ app/lib/api-client.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8000";
+  process.env.NEXT_PUBLIC_API_ENDPOINT ?? "http://localhost:8000";
 
 type FetchOptions = RequestInit & { skipAuthHeader?: boolean };
 
